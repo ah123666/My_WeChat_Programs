ca38ee8c4bd81f0d0d80dc651869d87aee778aa4
xyym <1920376753@qq.com>
2021-03-25 21:25:28 +0800

    11

--- demo/pages/shop/shop.js
+++ demo/pages/shop/shop.js
@@ -5,8 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
+    swiper_images: [
+    {url: "https://m.360buyimg.com/mobilecms/s700x280_jfs/t1/162082/21/14500/135739/605aab46Ecbdcac6d/e1ba2237e7bc7ce0.jpg!q70.jpg.dpg"},
+    {url: "https://m.360buyimg.com/mobilecms/s700x280_jfs/t1/170764/13/13979/173167/60583796E94e31fb9/cdb770c32bf381bf.png!q70.jpg.dpg"},
+    {url: "https://m.360buyimg.com/mobilecms/s700x280_jfs/t1/164527/4/9204/172306/603fd9e3E40d8d22f/d3e4d89226b6fb6f.jpg!cr_1125x445_0_171!q70.jpg.dpg"},
+    {url: "https://m.360buyimg.com/mobilecms/s700x280_jfs/t1/155798/39/17748/54070/6059f017Ed7da37ee/aec5e75ba665769d.jpg!q70.jpg.dpg"},
+    {url: "https://m.360buyimg.com/mobilecms/s700x280_jfs/t1/169125/1/14095/157944/605812eeEf517f8ac/68a4a698d1e6e22a.jpg!cr_1125x445_0_171!q70.jpg.dpg"},
+    {url: "https://m.360buyimg.com/mobilecms/s700x280_jfs/t1/158734/34/14355/56844/6054992dEfba5a3e0/b35661fbdbb2f0a7.jpg!q70.jpg.dpg"}
+  ],
+    icon_items: [
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/125678/35/5947/4868/5efbf28cEbf04a25a/e2bcc411170524f0.png", txt: "京东超市"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/135931/4/3281/5598/5efbf2c0Edbdc82c7/ed9861b4ddfb9f30.png", txt: "数码电器"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/140012/8/1804/3641/5efbf318E38bd5dad/0db99d859ab16ce9.png", txt: "京东服饰"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/129215/21/5978/3618/5efbf344Ebec23ae8/59712d986b10bb0a.png", txt: "京东生鲜"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/116602/7/11200/3796/5efbf375Ebba41029/f07cc166f368fa05.png", txt: "京东到家"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/146390/3/1846/4909/5efbf39aEe5f5f797/300071558a9ab078.png", txt: "充值缴费"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/150351/19/14167/6853/5fed882dE195ef673/b2aa7d67e675baf8.png", txt: "9.9元拼"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/113589/24/11332/4897/5efbf3feE705d87db/e5c12d5e943266b9.png", txt: "领券"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/129184/28/5977/3711/5efbf53aE2c2e6a07/7db529ce0e00838f.png", txt: "借钱"},
+    {image_url: "https://m.360buyimg.com/mobilecms/s120x120_jfs/t1/123730/37/5924/4189/5efbf567E0a226121/d04df7c74c87cf68.png", txt: "PLUS会员"},
+  ],
+  items1: [
+    {txt: "正品直邮", image_url: "https://m.360buyimg.com/mobilecms/s150x150_jfs/t1/122304/6/4867/26484/5ee73aa3E8a601402/04d440839b734589.png!q70.jpg.dpg"},
+    {txt: "3C大放价", image_url: "https://m.360buyimg.com/mobilecms/s150x150_jfs/t1/146350/29/713/33082/5ee73d9fE1ae995ad/0ec365679efbf615.png!q70.jpg.dpg"},
+    {txt: "国货正当时", image_url: "https://m.360buyimg.com/mobilecms/s150x150_jfs/t1/121172/17/1584/29106/5ebd1637Ec4c82232/aa56520b3ab276be.png!q70.jpg.dpg"},
+    {txt: "都是你爱的", image_url: "https://m.360buyimg.com/mobilecms/s150x150_jfs/t1/141856/19/761/28886/5ee73fa2E4401ba28/9386324f55097e7b.jpg!q70.jpg.dpg"}
+  ]
 
-  },
+},
 
   /**
    * 生命周期函数--监听页面加载

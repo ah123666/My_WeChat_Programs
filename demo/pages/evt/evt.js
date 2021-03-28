78c7a15c4456ea24156ed1e6de69b858ad6810d0
xyym <1920376753@qq.com>
2021-03-28 17:28:36 +0800

    111

---
+++ demo/pages/evt/evt.js
@@ -0,0 +1,76 @@
+// pages/evt/evt.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  // 自定义事件
+  click(evt){
+    console.log('目标：',evt.target.dataset)
+    console.log('当前触发对象：',evt.currentTarget.dataset)
+    // console.log(evt)
+    console.log("摸了！")
+  },
+  click2(evt){
+    console.log("子级！")
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
